21778172d90d8663cff6bea7289fcd9420843746
Herman <nitrowitty@gmail.com>
2024-09-01 08:34:07 -0600

    pointers and errors

--- main.go
+++ main.go
@@ -6,10 +6,21 @@ import (
 	"helloworld/hello"
 	"helloworld/integers"
 	"helloworld/iteration"
+	pointerserrors "helloworld/pointers_errors"
+	structsmethodsinterfaces "helloworld/structs_methods_interfaces"
 )
 
 func main() {
 	fmt.Println(hello.Hello("Heman", "Spanish"))
 	fmt.Println(integers.Add(2, 2))
 	fmt.Println(iteration.Repeat("a"))
+
+	rectangle := structsmethodsinterfaces.Rectangle{Height: 20, Width: 30}
+
+	fmt.Println(rectangle.Area())
+
+	wallet := pointerserrors.Wallet{}
+	wallet.Deposit(50)
+
+	fmt.Println(wallet.Balance())
 }
